b13e47e73fd42fa431a058e11cece0496d676dda
Luis David Licea Torres <luislicea1011@hotmail.com>
2022-04-09 00:10:38 -0600

    Add search bar button group.
    The button group ensures that we can only search Services, Tasks, or
    Users in a single search, in a mutually exclusive way. Also, the this
    commit adds an ordering drop down box for allowing the users to order
    the results. The ordering happens in the backend and is not yet
    implemented.

--- nifti/search/views.py
+++ nifti/search/views.py
@@ -7,50 +7,62 @@ from django.db.models import Q
 
 # Create your views here.
 def search(request):
-  #handle which queries to make based on checkboxes
-  print('\n\n' + str(request.GET.get('flexSwitchUser')) + '\n\n')
+  """Handle which queries to make based on checkboxes."""
 
-  if(request.GET and 
-    request.GET.get('search_bar') != "" and 
-    request.GET.get('search_bar') != None
-  ):
+  # Print all the variables in the request.
+  print(f'\n{request.GET}\n')
+
+  # Get either Service, Task, or User
+  search_type: str = request.GET.get('search_type')
+
+  # Get search string from search bar.
+  search_str: str = request.GET.get('search_bar')
+
+  # Get result ordering.
+  search_ord: str = request.GET.get('ordering')
+
+
+  # Verify that the query is not empty.
+  if(request.GET and search_str):
     results = True
-    search_type = ''
     users = ''
     posts = ''
     searched_for = ''
 
     #User Search
-    if(request.GET.get('flexSwitchUser') == "on"):
-      users = User.objects.filter(Q(username__icontains=request.GET.get('search_bar')))
-      search_type = 'user_search' #service_or_task_search, distance_search
+    if(search_type == "User"):
+      users = User.objects.filter(Q(username__icontains=search_str))
+      # service_or_task_search, distance_search
+      search_type = 'user_search'
       context = {
-        'search_type': search_type,
-        'users': users,
+          'search_type': search_type,
+          'users': users,
       }
       return render(request, 'search/search.html', context)
 
     #Service/Task Search
-    elif(request.GET.get('flexSwitchService') == "on" or request.GET.get('flexSwitchTask') == "on"):
+    elif(search_type == "Service" or search_type == "Task"):
       #service provider
-      if (request.GET.get('flexSwitchService') == "on"):
+      if (search_type == "Service"):
         #Check matching tags, then check posts associated with said tags, then get those posts.
-        tags = Tag.objects.filter(Q(tag_name__icontains=request.GET.get('search_bar')))
+        tags = Tag.objects.filter(Q(tag_name__icontains=search_str))
         posts = Post.objects.none()
         for tag in tags:
-          tag_to_post_query_set = TagToPostTable.objects.filter(Q(tag_id=tag.id)) 
+          tag_to_post_query_set = TagToPostTable.objects.filter(
+              Q(tag_id=tag.id))
           for tag_to_post in tag_to_post_query_set:
-            posts = posts | Post.objects.filter(Q(id=tag_to_post.post_id)) #adding to Post QuerySet
+            # adding to Post QuerySet
+            posts = posts | Post.objects.filter(
+                Q(id=tag_to_post.post_id))
 
         #TODO: now we want to pass into context a list of all tags associated with one post.
         #potentially double-array or a dictionary
 
       search_type = 'service_or_task_search'
       context = {
-        'search_type': search_type,
-        'posts': posts,
+          'search_type': search_type,
+          'posts': posts,
       }
       return render(request, 'search/search.html', context)
-
 
   return render(request, 'search/search.html')
